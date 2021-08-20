fb6a6794949338a803605d85fc9bc87d605732b2
jadx2 <delmarjaim@gmail.com>
2021-08-20 18:04:32 -0500

    Fix linter errors

--- app/lib/authentication_token.rb
+++ app/lib/authentication_token.rb
@@ -1,7 +1,7 @@
 # Service for creating token from JWT
 class AuthenticationToken
   HMAC_SECRET = Rails.application.secret_key_base
-  ALGORITHM_TYPE = 'HS256'
+  ALGORITHM_TYPE = 'HS256'.freeze
 
   def self.encode(user_id)
     payload = { user_id: user_id }
