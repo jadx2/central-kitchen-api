454b0b94fa6b4d7e99f8e58596e31eb7faa41671
jadx2 <delmarjaim@gmail.com>
2021-08-23 11:02:48 -0500

    Fix linter errors

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    raise(AuthenticationError, Message.invalid_credentials) unless user && user.authenticate(params[:password])
+    raise(AuthenticationError, Message.invalid_credentials) unless user&.authenticate(params[:password])
 
     token = AuthenticationToken.encode(user.id)
 
