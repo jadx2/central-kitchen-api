3657fcf56c89378964487ee4b2ce5970736dfaa2
jadx2 <delmarjaim@gmail.com>
2021-08-25 16:07:59 -0500

    Fix typing error in routes for only create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :workshops
   resources :attendances
-  resources :users, only: :create
-  resources :authentication, onlyt: :create
+  resources :users, only: [:create]
+  resources :authentication, only: [:create]
 end
